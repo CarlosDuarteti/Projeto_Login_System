75a95c70e895bb9d58e7b0e46dd634d930f03809
CarlosDuarteti <carloshduarte123@gmail.com>
2026-08-28 21:30:43 -0300

    Criando o arquivo register.js

--- backend/routers/auth.js
+++ backend/routers/auth.js
@@ -55,7 +55,7 @@ message: "Erro no servidor ao registrar."
 router.post("/login", async (req, res) => {
 try {const { email, senha } = req.body;
 
-if (!email & !senha) {
+if (!email && !senha) {
 return res.status(400).json({
 message: "Preencha todos os campos."});
 }if (!email) {
